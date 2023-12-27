97ea01ead3cb99f055f48e6bc578ff19ee6a228e
Norbz27 <128601526+Norbz27@users.noreply.github.com>
2023-12-27 22:02:41 +0800

    j

--- AMSEMS/SQL_Connection.cs
+++ AMSEMS/SQL_Connection.cs
@@ -9,14 +9,14 @@
         //public static string connection = @"Data Source=LAPTOP-78S661F5;Initial Catalog=db_Amsems;Integrated Security=True";
 
         //Connect locally
-        public static string connection = @"Server=192.168.1.100;Database=db_Amsems;User Id=nor;Password=12345;";
+        //public static string connection = @"Server=192.168.1.100;Database=db_Amsems;User Id=nor;Password=12345;";
 
         //ngrok
-        //static string server = "0.tcp.ap.ngrok.io,12162"; // Ngrok tunnel URL
-        //static string database = "db_Amsems"; // Database name
-        //static string username = "nor"; // Database username
-        //static string password = "12345"; // Database password
+        static string server = "0.tcp.ap.ngrok.io,18065"; // Ngrok tunnel URL
+        static string database = "db_Amsems"; // Database name
+        static string username = "nor"; // Database username
+        static string password = "12345"; // Database password
 
-        //public static string connection = $"Server={server};Database={database};User Id={username};Password={password};";
+        public static string connection = $"Server={server};Database={database};User Id={username};Password={password};";
     }
 }
