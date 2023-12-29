c40a1b883a8320b78327a0d6db70d27b534978b7
Norbz27 <128601526+Norbz27@users.noreply.github.com>
2023-12-30 01:45:36 +0800

    connection update

--- AMSEMS_Attendance_Checker/SQL_Connection.cs
+++ AMSEMS_Attendance_Checker/SQL_Connection.cs
@@ -2,8 +2,11 @@
 {
     internal class SQL_Connection
     {
-        //cloud
-        //public static string connection = @"Server=tcp:norbz.database.windows.net,1433;Initial Catalog=db_Amsems;Persist Security Info=False;User ID=CloudSA4a47677a@norbz;Password=nozurbnorberto27@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;";
+        //azure cloud
+        //public static string connection = @"Server=tcp:amsems.database.windows.net,1433;Initial Catalog=db_Amsems;Persist Security Info=False;User ID=CloudSA230f235e@amsems;Password=Nozurbnorberto27;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;";
+
+        //google cloud
+        public static string connection = @"Server=104.197.95.130;Database=db_Amsems;User Id=sqlserver;Password=Nozurbnorberto27;";
 
         //local
         //public static string connection = @"Data Source=LAPTOP-78S661F5;Initial Catalog=db_Amsems;Integrated Security=True";
@@ -12,12 +15,12 @@
         //public static string connection = @"Server=192.168.1.100;Database=db_Amsems;User Id=nor;Password=12345;";
 
         //ngrok
-        static string server = "0.tcp.ap.ngrok.io,18065"; // Ngrok tunnel URL
-        static string database = "db_Amsems"; // Database name
-        static string username = "nor"; // Database username
-        static string password = "12345"; // Database password
+        //static string server = "0.tcp.ap.ngrok.io,18065"; // Ngrok tunnel URL
+        //static string database = "db_Amsems"; // Database name
+        //static string username = "nor"; // Database username
+        //static string password = "12345"; // Database password
 
-        public static string connection = $"Server={server};Database={database};User Id={username};Password={password};";
+        //public static string connection = $"Server={server};Database={database};User Id={username};Password={password};";
 
     }
 }
